0144e5aed3baabf735c68db8f6a908d64e4bbce5
ur13l <ur13l.infante@gmail.com>
2014-12-18 19:31:09 -0600

    Chapter 13: Challenge

--- app/src/main/java/com/projects/ur13l/hellomoon/AudioPlayer.java
+++ app/src/main/java/com/projects/ur13l/hellomoon/AudioPlayer.java
@@ -8,27 +8,43 @@ import android.media.MediaPlayer;
  */
 public class AudioPlayer {
     private MediaPlayer mPlayer;
+    private boolean mPaused;
 
     public void stop(){
         if( mPlayer != null){
             mPlayer.release();
             mPlayer = null;
+            mPaused = false;
         }
     }
 
     public void play(Context c){
-        stop();
-        mPlayer = MediaPlayer.create(c,R.raw.one_small_step);
+
+        if(!isPaused()) {
+            stop();
+
+       mPlayer = MediaPlayer.create(c,R.raw.one_small_step);
         mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
             @Override
             public void onCompletion(MediaPlayer mp) {
                 stop();
             }
         });
+        }
         mPlayer.start();
+        mPaused = false;
     }
 
     public void pause(){
+        if(mPlayer != null) {
+            if (mPlayer.isPlaying()) {
+                mPlayer.pause();
+                mPaused = true;
+            }
+        }
+    }
 
+    public boolean isPaused(){
+        return mPaused;
     }
 }
--- app/src/main/java/com/projects/ur13l/hellomoon/HelloMoonFragment.java
+++ app/src/main/java/com/projects/ur13l/hellomoon/HelloMoonFragment.java
@@ -14,6 +14,7 @@ import android.widget.Button;
 public class HelloMoonFragment extends Fragment {
     private AudioPlayer mPlayer = new AudioPlayer();
     private Button mPlayButton;
+    private Button mPauseButton;
     private Button mStopButton;
 
 
@@ -25,6 +26,14 @@ public class HelloMoonFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 mPlayer.play(getActivity());
+            }
+        });
+
+        mPauseButton = (Button) v.findViewById(R.id.hellomoon_pauseButton);
+        mPauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mPlayer.pause();
             }
         });
 
